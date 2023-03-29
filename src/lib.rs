5c7e47bd4f47543e64ad43b1195313b0ddeb0167
Ji Chen <jaykchen@gmail.com>
2023-03-29 15:13:30 -0400

    store flow, send to slack

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,78 @@
 use airtable_flows::create_record;
-use github_flows::{
-    get_octo, listen_to_event,
-    octocrab::Result,
-    octocrab::{self},
-    EventPayload,
-};
+use chrono::{DateTime, Utc};
+use http_req::request;
 use schedule_flows::schedule_cron_job;
 use serde::Deserialize;
+use serde_json::Value;
 use slack_flows::send_message_to_channel;
+use store_flows::{global_get, global_set};
 
 #[no_mangle]
-#[tokio::main(flavor = "current_thread")]
-pub async fn run() {
-    let login = "jaykchen";
-    let owner = "jaykchen";
-    let repo = "vitesse-lite";
-
-    listen_to_event(login, owner, repo, vec!["pull_request"], |payload| {
-        handler(login, owner, repo, payload)
-    })
-    .await;
+pub fn run() {
+    schedule_cron_job(
+        String::from("15 * * * *"),
+        String::from("cron_job_evoked"),
+        callback,
+    );
 }
 
-async fn handler(login: &str, owner: &str, repo: &str, payload: EventPayload) {
+fn callback(_body: Vec<u8>) {
+    let search_key_word = "WASMEDGE";
     let account: &str = "jaykchen";
     let base_id: &str = "apptJFYvsGrrywvWh";
     let table_name: &str = "users";
 
-    let octocrab = get_octo(Some(String::from(login)));
+    let URI =
+        format!("https://api.github.com/search/issues?q={search_key_word}&sort=created&order=desc");
 
-    let query_str = "search/issues?q=WASMEDGE&sort=created&order=desc".to_string();
+    let mut writer = Vec::new();
+    let result = request::get(URI, &mut writer);
 
-    send_message_to_channel("ik8", "ch_in", query_str.to_string());
+    if let Ok(resp) = result {
+        if resp.status_code().is_success() {
+            let res: Result<SearchResult, _> = serde_json::from_slice(&writer);
 
-    let response: Result<SearchResult, octocrab::Error> =
-        octocrab.get(&query_str, None::<&()>).await;
+            match res {
+                Err(_e) => {}
 
-    match response {
-        Err(e) => {}
+                Ok(response) => {
+                    let time_entries_last_saved: DateTime<Utc> =
+                        global_get("time_entries_last_saved")
+                            .unwrap()
+                            .to_string()
+                            .parse()
+                            .expect("Invalid date-time format");
+                    send_message_to_channel("ik8", "ch_in", time_entries_last_saved.to_string());
+                    for item in response.items {
+                        let name = item.user.login;
+                        let title = item.title;
+                        send_message_to_channel("ik8", "general", title.to_string());
+                        let html_url = item.html_url;
+                        let time = item.created_at;
 
-        Ok(response) => {
-            for item in response.items {
-                let name = item.user.login;
-                let title = item.title;
-                send_message_to_channel("ik8", "general", title.to_string());
-                let html_url = item.html_url;
-                let time = item.created_at;
+                        if time > time_entries_last_saved {
+                            let text = format!(
+                                "{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}"
+                            );
+                            send_message_to_channel("ik8", "general", text);
 
-                let text =
-                    format!("{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}");
-                send_message_to_channel("ik8", "general", text);
-
-                let data = serde_json::json!({
-                    "Name": name,
-                    "Repo": html_url,
-                    "Created": time,
-                });
-                create_record(account, base_id, table_name, data);
+                            let data = serde_json::json!({
+                                "Name": name,
+                                "Repo": html_url,
+                                "Created": time,
+                            });
+                            create_record(account, base_id, table_name, data.clone());
+                            let time: Value =
+                                serde_json::to_value(&time).expect("Failed to serialize date-time");
+                            global_set("time_entries_last_saved", time);
+                            send_message_to_channel("ik8", "ch_out", data.to_string());
+                        }
+                        send_message_to_channel("ik8", "ch_mid", time.to_string());
+                    }
+                }
             }
         }
-    };
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -72,7 +85,7 @@ struct Issue {
     html_url: String,
     title: String,
     user: User,
-    created_at: String,
+    created_at: DateTime<Utc>,
 }
 
 #[derive(Debug, Deserialize)]
