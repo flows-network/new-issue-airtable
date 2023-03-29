b0795f6953a983f145d9eb2ce84ecee18f05c1b8
Ji Chen <jaykchen@gmail.com>
2023-03-29 17:31:00 -0400

    http_req, Request

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use airtable_flows::create_record;
 use chrono::{DateTime, Utc};
-use http_req::request;
+use dotenv::dotenv;
+use http_req::{request::Request, response::Headers, uri::Uri};
 use schedule_flows::schedule_cron_job;
 use serde::Deserialize;
 use serde_json::Value;
 use slack_flows::send_message_to_channel;
+use std::convert::TryFrom;
+use std::env;
 use store_flows::{global_get, global_set};
 
 #[no_mangle]
@@ -17,64 +20,76 @@ pub fn run() {
 }
 
 fn callback(_body: Vec<u8>) {
-    let search_key_word = "WASMEDGE";
+    dotenv().ok();
+
+    let github_token: String =
+        env::var("github_token").unwrap_or("GitHub token not found".to_string());
     let account: &str = "jaykchen";
     let base_id: &str = "apptJFYvsGrrywvWh";
     let table_name: &str = "users";
 
-    let URI =
-        format!("https://api.github.com/search/issues?q={search_key_word}&sort=created&order=desc");
+    let search_key_word = "GitHub WASMEDGE";
 
-    let mut writer = Vec::new();
-    let result = request::get(URI, &mut writer);
+    let url_str = format!(
+        "https://api.github.com/search/issues?q={}&sort=created&order=desc",
+        search_key_word
+    );
+    let bearer_token = format!("bearer {}", github_token);
+    let url = Uri::try_from(url_str.as_str()).unwrap();
 
-    if let Ok(resp) = result {
-        if resp.status_code().is_success() {
-            let res: Result<SearchResult, _> = serde_json::from_slice(&writer);
+    let mut writer = Vec::<u8>::new();
 
-            match res {
-                Err(_e) => {}
+    let mut headers = Headers::new();
+    headers.insert("User-Agent", "flows-network connector");
+    headers.insert("Authorization", &bearer_token);
+    headers.insert("Accept", "application/vnd.github+json");
 
-                Ok(response) => {
-                    let time_entries_last_saved: Option<DateTime<Utc>> =
-                        match global_get("time_entries_last_saved")
-                            .unwrap()
-                            .to_string()
-                            .parse()
-                        {
-                            Ok(t) => Some(t),
-                            Err(_) => None,
-                        };
-                    for item in response.items {
-                        let name = item.user.login;
-                        let title = item.title;
-                        send_message_to_channel("ik8", "general", title.to_string());
-                        let html_url = item.html_url;
-                        let time = item.created_at;
+    Request::new(&url)
+        .headers(headers)
+        .send(&mut writer)
+        .unwrap();
 
-                        if time_entries_last_saved.is_none()
-                            || time_entries_last_saved.is_some()
-                                && time > time_entries_last_saved.unwrap()
-                        {
-                            let text = format!(
-                                "{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}"
-                            );
-                            send_message_to_channel("ik8", "general", text);
+    let res: Result<SearchResult, _> = serde_json::from_slice(&writer);
 
-                            let data = serde_json::json!({
-                                "Name": name,
-                                "Repo": html_url,
-                                "Created": time,
-                            });
-                            create_record(account, base_id, table_name, data.clone());
-                            let time_serialized: Value =
-                                serde_json::to_value(&time).expect("Failed to serialize date-time");
-                            global_set("time_entries_last_saved", time_serialized);
-                            send_message_to_channel("ik8", "ch_out", data.to_string());
-                        }
-                        send_message_to_channel("ik8", "ch_mid", time.to_string());
-                    }
+    match res {
+        Err(_e) => {}
+
+        Ok(search_result) => {
+            let time_entries_last_saved: Option<DateTime<Utc>> =
+                match global_get("time_entries_last_saved")
+                    .unwrap()
+                    .to_string()
+                    .parse()
+                {
+                    Ok(t) => Some(t),
+                    Err(_) => None,
+                };
+            for item in search_result.items {
+                let name = item.user.login;
+                let title = item.title;
+                send_message_to_channel("ik8", "general", title.to_string());
+                let html_url = item.html_url;
+                let time = item.created_at;
+
+                if time_entries_last_saved.is_none()
+                    || time_entries_last_saved.is_some() && time > time_entries_last_saved.unwrap()
+                {
+                    let text =
+                        format!("{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}");
+                    send_message_to_channel("ik8", "general", text);
+
+                    let data = serde_json::json!({
+                        "Name": name,
+                        "Repo": html_url,
+                        "Created": time,
+                    });
+                    create_record(account, base_id, table_name, data.clone());
+                    let time_serialized: Value =
+                        serde_json::to_value(&time).expect("Failed to serialize date-time");
+                    global_set("time_entries_last_saved", time_serialized);
+                    send_message_to_channel("ik8", "ch_out", data.to_string());
                 }
+                send_message_to_channel("ik8", "ch_mid", time.to_string());
             }
         }
     }
@@ -90,7 +105,7 @@ struct Issue {
     html_url: String,
     title: String,
     user: User,
-    created_at: DateTime::<Utc>,
+    created_at: DateTime<Utc>,
 }
 
 #[derive(Debug, Deserialize)]
