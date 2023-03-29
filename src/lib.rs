41326f4fc929c1f6319b15534431e1b96c176be7
Ji Chen <jaykchen@gmail.com>
2023-03-28 21:03:58 -0400

    by gpt

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,78 @@
 use airtable_flows::create_record;
-use flowsnet_platform_sdk::write_error_log;
-use github_flows::{get_octo, octocrab};
+use github_flows::{
+    get_octo,
+    octocrab::Result,
+    octocrab::{self},
+};
 use schedule_flows::schedule_cron_job;
+use serde::Deserialize;
 use slack_flows::send_message_to_channel;
-use tokio::spawn;
+
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     schedule_cron_job(
-        String::from("30 @hourly * * *"),
+        String::from("25 * * * *"),
         String::from("cron_job_evoked"),
-        |body| {
-            spawn(async {
+        Box::new(|body: Vec<u8>| {
+            Box::pin(async move {
                 let login: &str = "jaykchen";
                 let account: &str = "jaykchen";
                 let base_id: &str = "apptJFYvsGrrywvWh";
                 let table_name: &str = "users";
-
+                
                 let octocrab = get_octo(Some(String::from(login)));
-
-                let pages = octocrab::instance()
-                    .search()
-                    .issues_and_pull_requests("GitHub WASMEDGE")
-                    .sort("comments")
-                    .order("asc")
-                    .send()
-                    .await;
-
-                match pages {
-                    Err(_e) => {
-                        write_error_log!( _e.to_string());
-                    }
-
-                    Ok(pages) => {
-                        for item in pages {
-                            let name = item.author_association;
+                
+                let query_str = "search/issues?q=WASMEDGE&sort=created&order=desc".to_string();
+                
+                send_message_to_channel("ik8", "ch_in", query_str.to_string());
+                
+                let response: Result<SearchResult, octocrab::Error> =
+                octocrab.get(&query_str, None::<&()>).await;
+                
+                match response {
+                    Err(e) => {}
+                    
+                    Ok(response) => {
+                        for item in response.items {
+                            let name = item.user.login;
+                            let title = item.title;
                             let html_url = item.html_url;
                             let time = item.created_at;
-
-                            let text = format!("{name} mentioned WASMEDGE  @{html_url}\n{time}");
+                            
+                            let text = format!("{name} mentioned WASMEDGE in issue: {title}  @{html_url}\n{time}");
                             send_message_to_channel("ik8", "general", text);
-
+                            
                             let data = serde_json::json!({
-                            "Name": name,
-                            "Repo": html_url,
-                            "Created": time,
+                                "Name": name,
+                                "Repo": html_url,
+                                "Created": time,
                             });
-                            create_record(account, base_id, table_name, data)
+                            create_record(account, base_id, table_name, data);
                         }
                     }
-                }
+                };
+                ()
             });
-        },
+            ()
+        }),
     );
+}
+#[derive(Debug, Deserialize)]
+struct SearchResult {
+    items: Vec<Issue>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Issue {
+    html_url: String,
+    title: String,
+    user: User,
+    created_at: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct User {
+    login: String,
 }
